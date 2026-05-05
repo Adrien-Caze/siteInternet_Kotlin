abfa610ec184a893c22f1393ca9290689a32a353
Adrien <adrien.caze77@gmail.com>
2026-05-05 15:20:16 +0200

    Hello world fonctionnelle

---
+++ src/README.md
@@ -0,0 +1,7 @@
+
+import java.awt.Desktop // Permet d'ouvrir le navigateur par défaut
+import java.net.URI // Représente une adresse web
+import com.sun.net.httpserver.HttpServer // Serveur HTTP
+import com.sun.net.httpserver.HttpHandler // Gestionnaire de requêtes HTTP
+import com.sun.net.httpserver.HttpExchange // Représente une requête/réponse HTTP
+import java.net.InetSocketAddress // Adresse IP et port du serveur
--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,25 @@
 import java.awt.Desktop
 import java.net.URI
+import com.sun.net.httpserver.HttpServer
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpExchange
+import java.net.InetSocketAddress
 
 fun main() {
-    if (Desktop.isDesktopSupported()) {
-        val desktop = Desktop.getDesktop()
-        desktop.browse(URI.create("http://www.youtube.com"))
+    val serveur = HttpServer.create(InetSocketAddress(8080),0)
+    serveur.createContext("/Accueil"){ exchange ->
+        val reponse = """
+            <!DOCTYPE html>
+            <html lang="fr">
+                <h1>Hello World !<!h1>
+            </html>
+        """.trimIndent()
+        exchange.sendResponseHeaders(200,reponse.toByteArray().size.toLong())
+        val os = exchange.responseBody
+        os.write(reponse.toByteArray())
+        os.close()
     }
+    /*serveur.executor = null*/
+    serveur.start()
+    println("http://localhost:8080/Accueil")
 }
