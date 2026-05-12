d609e5a259bee72c64e79afdec9bfd8d91b8fbac
Adrien <adrien.caze77@gmail.com>
2026-05-12 15:19:28 +0200

    Ajout du contact form

--- src/main.kt
+++ src/main.kt
@@ -78,6 +78,53 @@ fun main() {
                         <h1>Hello World !</h1>
                         <p>Bienvenue sur mon site créé en Kotlin</p>
                     </div>
+                    <section class="py-5">
+                <div class="container px-5">
+                    <!-- Contact form-->
+                    <div class="bg-light rounded-4 py-5 px-4 px-md-5">
+                        <div class="text-center mb-5">
+                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 mb-3"><i class="bi bi-envelope"></i></div>
+                            <h1 class="fw-bolder">Contactez moi :</h1>
+                            <p class="lead fw-normal text-muted mb-0">Travaillons Ensemble !</p>
+                        </div>
+                        <div class="row gx-5 justify-content-center">
+                            <div class="col-lg-8 col-xl-6">
+                                <!-- * * * * * * * * * * * * * * *-->
+                                <!-- * * SB Forms Contact Form * *-->
+                                <!-- * * * * * * * * * * * * * * *-->
+                                <!-- This form is pre-integrated with SB Forms.-->
+                                <!-- To make this form functional, sign up at-->
+                                <!-- https://startbootstrap.com/solution/contact-forms-->
+                                <!-- to get an API token!-->
+                                <form action="https://formspree.io/f/xvgdkbgb" method="POST">
+    <div class="form-floating mb-3">
+        <input class="form-control" name="name" type="text" placeholder="Nom" required>
+        <label>Nom Complet</label>
+    </div>
+
+    <div class="form-floating mb-3">
+        <input class="form-control" name="email" type="email" placeholder="email" required>
+        <label>Adresse Email</label>
+    </div>
+
+    <div class="form-floating mb-3">
+        <input class="form-control" name="phone" type="text" placeholder="Téléphone" required>
+        <label>Numéro de téléphone</label>
+    </div>
+
+    <div class="form-floating mb-3">
+        <textarea class="form-control" name="message" placeholder="Votre message" style="height: 10rem" required></textarea>
+        <label>Message</label>
+    </div>
+
+    <button class="btn btn-primary btn-lg" type="submit">Envoyer</button>
+</form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
                 </body>
                 ${footer.render()}
             </html>
